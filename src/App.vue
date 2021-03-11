25f1f56aa7437b247a9ab20e5e62418da26add8c
Zack Holmberg <zack_holmberg@outlook.com>
2021-03-10 21:39:36 -0600

    Work

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,127 @@
 <template>
   <div>
     <input v-model="tag" class="form-control" required />
-    <button @click="searchTag(tag)">
+    <button class="searchBtn" @click="searchTag(tag)">
       Look up tag!
     </button>
   </div>
+  <br /><br />
+  <div v-for="item in toDisplay" :key="item">
+    <Collapsible :object="item" />
+  </div>
+
+  <h3 style="color:darkgreen;">Time taken: {{ timeTaken }} seconds</h3>
 </template>
 
 <script>
+import Collapsible from "./components/Collapsible.vue";
+
 export default {
   name: "App",
+  components: {
+    Collapsible,
+  },
+  data: function() {
+    return { toDisplay: [], timeTaken: 0 };
+  },
   methods: {
     searchTag(tag) {
-      console.log(tag);
+      // Get a date object for the current time
+      var d = new Date();
+
+      // Set it to one month ago
+      d.setDate(d.getDay() - 7);
+
+      var unix = Math.round(+d / 1000);
+
+      let mostVoted = [];
+      let newest = [];
+      let i;
+
+      const t0 = performance.now();
+
       this.axios
         .get(
-          "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=" +
-            encodeURIComponent(tag)
+          "https://api.stackexchange.com/2.2/search?fromdate=" +
+            unix +
+            "&order=desc&sort=creation&tagged=" +
+            encodeURIComponent(tag) +
+            "&site=stackoverflow&filter=!)c5QMjAfN_fpNUj3OeFii.L4mQGv4nt0aIVq6goV3lhcH"
         )
-        .then((response) => console.log(response));
+        .then((response) => {
+          let toFilter = response.data.items;
+          for (i = 0; i < 10 && i < toFilter.length; i++) {
+            newest[i] = toFilter[i];
+          }
+
+          this.axios
+            .get(
+              "https://api.stackexchange.com/2.2/search?fromdate=" +
+                unix +
+                "&order=desc&sort=votes&tagged=" +
+                encodeURIComponent(tag) +
+                "&site=stackoverflow&filter=!)c5QMjAfN_fpNUj3OeFii.L4mQGv4nt0aIVq6goV3lhcH"
+            )
+            .then((response) => {
+              let toFilter = response.data.items;
+              var i;
+              for (i = 0; i < 10 && i < toFilter.length; i++) {
+                mostVoted[i] = toFilter[i];
+              }
+
+              // ===========================================
+              // ADD TO THE TODISPLAY LIST
+              // ===========================================
+
+              for (i = 0; i < 10 && i < mostVoted.length; i++) {
+                let temp = mostVoted[i];
+
+                const found = this.toDisplay.some(
+                  (el) => el.question_id === temp.question_id
+                );
+                if (!found) {
+                  this.toDisplay[i] = mostVoted[i];
+                }
+              }
+
+              const currLength = mostVoted.length;
+              for (i = 0; i < 10 && i < newest.length; i++) {
+                let temp = newest[i];
+
+                const found = this.toDisplay.some(
+                  (el) => el.question_id === temp.question_id
+                );
+                if (!found) {
+                  this.toDisplay[i + currLength] = newest[i];
+                }
+              }
+
+              // ===========================================
+              // SORT LIST IN DESCENDING ORDER
+              // ===========================================
+
+              this.toDisplay.sort(
+                (a, b) =>
+                  parseFloat(b.creation_date) - parseFloat(a.creation_date)
+              );
+
+              // ===========================================
+              // CHANGE DATE TO SOMETHING HUMAN READABLE
+              // ===========================================
+
+              for (i = 0; i < this.toDisplay.length; i++) {
+                const creationDate = new Date(
+                  this.toDisplay[i].creation_date * 1000
+                ).toLocaleString();
+                this.toDisplay[i].creation_date = creationDate;
+              }
+
+              console.log("To Display:", this.toDisplay);
+              const t1 = performance.now();
+
+              this.timeTaken = (t1 - t0) / 1000;
+            });
+        });
     },
   },
 };
@@ -32,5 +135,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background-color: darkgrey;
+}
+
+input {
+  border-radius: 25px;
+  width: 5%;
+  margin-right: 25px;
+  padding: 10px;
+  border: 2px solid black;
+}
+
+.searchBtn {
+  border-radius: 25px;
+  border: 2px solid black;
+  width: 100px;
+  padding: 10px;
+  background-color: lightgoldenrodyellow;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Collapsible.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <button @click="setActive()" type="button" class="collapsible">
+      <h3>{{ object.title }}</h3>
+      <b>Created on:</b> {{ object.creation_date }} <b>Score:</b>
+      {{ object.score }}
+    </button>
+    <div v-if="active" class="content">
+      <h1>{{ object.title }}</h1>
+      <span v-html="object.body"></span>
+      <h3>Comments</h3>
+      <div v-for="item in object.comments" :key="item">
+        <b>Created on:</b> {{ item.creation_date }} <b>Score:</b>
+        {{ item.score }} <b>Content: </b>
+        <span v-html="item.body"></span>
+      </div>
+
+      <h2>Answers</h2>
+      <div v-for="item in object.answers" :key="item">
+        <b>Created on:</b> {{ item.creation_date }} <b>Score:</b>
+        {{ item.score }} <b>Content: </b>
+        <span v-html="item.body"></span>
+        <h4>Answer Comments</h4>
+        <div v-for="item in item.comments" :key="item">
+          <b>Created on:</b> {{ item.creation_date }} <b>Score:</b>
+          {{ item.score }} <b>Content: </b>
+          <span v-html="item.body"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Collapsible",
+  data: function() {
+    return { active: false };
+  },
+  props: {
+    object: Object,
+  },
+  methods: {
+    setActive() {
+      console.log("Hitting this mf");
+      this.active = !this.active;
+    },
+  },
+};
+</script>
+
+<style>
+/* Style the button that is used to open and close the collapsible content */
+.collapsible {
+  background-color: lightgrey;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: 3px solid grey;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active,
+.collapsible:hover {
+  background-color: lightblue;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.content {
+  padding: 20px;
+  margin: 20px;
+
+  background-color: lightcyan;
+  width: 100%;
+}
+</style>
